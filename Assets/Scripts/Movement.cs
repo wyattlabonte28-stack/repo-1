84d953958f463b0ca197108eb1a480f2a9d3d4ed
wyattlabonte28-stack <wyatt.labonte28@thorntonacademy.org>
2026-04-30 13:02:09 -0400

    defv

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+
+public class Movement : MonoBehaviour
+
+{
+    public float speed = 5.0f;
+    public float horizontalInput;
+    public float verticalInput;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+     void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput= Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+        }
+        }
+    }
+
