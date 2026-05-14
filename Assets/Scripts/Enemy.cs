cd17bb5bd2334ed932934d270a2bb23f149f37b0
wyattlabonte28-stack <wyatt.labonte28@thorntonacademy.org>
2026-05-14 13:21:37 -0400

    dd

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,6 +7,7 @@ public class Enemy : MonoBehaviour
     public Transform player; // Drag the player here in the inspector
     public float moveForce = 10f;
     private Rigidbody rb;
+    public GameObject pro;
 
 
     void Start()
@@ -35,10 +36,19 @@ public class Enemy : MonoBehaviour
 private void OnCollisionEnter(Collision collision)
     {
         // Check if the collided object has the "Ground" tag
-        if (collision.gameObject.CompareTag("Ground"))
+        if (collision.gameObject.CompareTag("Projectile"))
         {
             Debug.Log("Enemy hit the ground!");
+                        Destroy(gameObject);
+            Destroy(pro.gameObject);
             // Insert your landing logic here (e.g., stop falling animation)
         }
     }
+  //          if (collision.gameObject.CompareTag("Enemy"))
+  //      {
+   //         Destroy(gameObject);
+   //         Destroy(Enemy.gameObject);
+   //         Debug.Log("Enemy got hit!");
+   //         // Insert your landing logic here (e.g., stop falling animation)
+  //      }
 }
