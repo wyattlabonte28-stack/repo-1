1fb85a750fe43a4836d3cb63babdcb91d5c07226
wyattlabonte28-stack <wyatt.labonte28@thorntonacademy.org>
2026-05-18 13:14:25 -0400

    ww

--- Assets/Scripts/ProMove.cs
+++ Assets/Scripts/ProMove.cs
@@ -1,14 +1,18 @@
+
+
 using UnityEngine;
 
 public class ProMove : MonoBehaviour
 {
     public float speed = 5.0f;
     public float delete = 50; 
+
     public GameObject Enemy;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         transform.Rotate(0, 270, 0);
+
     }
 
     // Update is called once per frame
@@ -27,6 +31,7 @@ public class ProMove : MonoBehaviour
             Debug.Log("Enemy got hit!");
             // Insert your landing logic here (e.g., stop falling animation)
         }
+
     }
 
 }
