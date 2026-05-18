540c97b656adf42d1120988f9e292549d17a7853
wesleystepp28-lang <wesley.stepp28@thorntonacademy.org>
2026-05-18 13:14:46 -0400

    DAY 6

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    public float speed = 10.0f;
-    public GameObject[] projectilePrefab;
+   
+    public GameObject projectilePrefab;
     public GameObject gunRotation;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,8 +20,8 @@ public class Projectile : MonoBehaviour
 
      if (Input.GetKeyDown(KeyCode.Mouse0))
      {
-                      int projectileIndex = Random.Range(0, projectilePrefab.Length);
-        Instantiate(projectilePrefab[projectileIndex], transform.position, gunRotation.transform.rotation);
+                      
+        Instantiate(projectilePrefab, transform.position, gunRotation.transform.rotation);
 
      }   
     }
