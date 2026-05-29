5156fe44c94ad7d244a46443843dfc20779d074c
giovannimontecalv26-del <giovanni.montecalv26@thorntonacademy.org>
2026-05-29 12:34:46 -0400

    gun sounds

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -8,10 +8,13 @@ public class Projectile : MonoBehaviour
     public GameObject projectilePrefab;
     public GameObject tipOfBarrel;
     public GameObject gunRotation;
+    public AudioClip GunSound;
+    private AudioSource ProjectileAudio;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        ProjectileAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -23,9 +26,9 @@ public class Projectile : MonoBehaviour
 
      if (Input.GetKeyDown(KeyCode.Mouse0))
      {
-                      
+                   ProjectileAudio.PlayOneShot(GunSound, 1.0f);   
         Instantiate(projectilePrefab, tipOfBarrel.transform.position, gunRotation.transform.rotation);
-
+        
      }   
     }
 }
